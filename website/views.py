a6b6fd9b7d0f6bd655959f30202902906ccf1069
Patrick <95184328+pat8901@users.noreply.github.com>
2022-10-28 21:49:14 -0400

    10/28/22 updated base program

--- website/views.py
+++ website/views.py
@@ -8,15 +8,3 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template("home.html")
-
-@views.route("/login")
-def login():
-    return render_template("login.html")
-
-@views.route("/logout")
-def logout():
-    return render_template("logout.html")
-
-@views.route("/sign-up")
-def sign_up():
-    return render_template("signUp.html")
